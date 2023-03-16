797789ee82434815773b74a1e2cb983411f4060e
CloudDecimo <112643666+CloudDecimo@users.noreply.github.com>
2023-03-16 18:04:53 +0400

    create provider config

--- main.tf
+++ main.tf
@@ -1,2 +1,16 @@
 # terraform-repo
 for terraform testing activities
+
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 3.0"
+   }
+ }
+}
+
+# Provider is AWS.
+provider "aws" {
+  region     = "us-east-1"
+}
